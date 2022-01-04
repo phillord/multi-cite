6d93aa1f78f14e6415f4fc7e42bf00e04ef0d90f
Phillip Lord <phillip.lord@russet.org.uk>
2022-01-04 17:43:09 +0000

    Ensure bibdatabase when no existing file

--- multi_cite/bib.py
+++ multi_cite/bib.py
@@ -3,30 +3,34 @@ from .util import eprint
 import bibtexparser
 import os
 
-bibliography_file="__multi_cite.bib"
-bibtex_database=None
-def bibtex_database():
-    if os.path.exists(bibliography_file):
-        with open(bibliography_file) as bibtex_file:
-            bibtex_database = bibtexparser.load(bibtex_file)
+__bibliography_file="__multi_cite.bib"
+__bibtex_database=None
 
-    return bibtex_database
+def bibtex_database():
+    global __bibtex_database
+    if os.path.exists(__bibliography_file):
+        with open(__bibliography_file) as bibtex_file:
+            __bibtex_database = bibtexparser.load(bibtex_file)
+    else:
+        __bibtex_database = bibtexparser.bibdatabase.BibDatabase()
+
+    return __bibtex_database
 
 def bib_filter(elem, doc):
     if type(elem) == MetaMap:
         bib = elem.content.get("bibliography")
         if not bib:
             elem.content["bibliography"] = MetaList(
-                MetaInlines(Str(bibliography_file))
+                MetaInlines(Str(__bibliography_file))
             )
             return elem
 
-        if not MetaInlines(Str(bibliography_file)) in bib:
-            bib.append(MetaInlines(Str(bibliography_file)))
+        if not MetaInlines(Str(__bibliography_file)) in bib:
+            bib.append(MetaInlines(Str(__bibliography_file)))
             return elem
 
 def push_to_bib(new_references):
-    with open(bibliography_file, "a") as f:
+    with open(__bibliography_file, "a") as f:
         f.write(new_references)
 
-    eprint("Updated ",bibliography_file)
+    eprint("Updated ", __bibliography_file)
