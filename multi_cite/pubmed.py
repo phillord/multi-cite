845cdc02a5a76c9533aedf0e5582f56857a95edd
Phillip Lord <phillip.lord@russet.org.uk>
2022-06-18 17:51:57 +0100

    Use DOI to resolve pmid

--- multi_cite/pubmed.py
+++ multi_cite/pubmed.py
@@ -18,44 +18,10 @@ def pmid_to_xml(pmid):
             pmid
         )
     )
-    eprint(r.text)
+
     root = ET.fromstring(r.text)
 
     return root
-
-def pmid_to_bibtex(pmid):
-    root = pmid_to_xml(pmid)
-
-    authors=[]
-    for auth in root.findall(".//AuthorList/Author"):
-        authors.append (
-            auth.find("ForeName").text +
-            " " + auth.find("LastName").text
-        )
-
-    authors = " and ".join(authors)
-    title = root.find(".//ArticleTitle").text
-    year = root.find(".//ArticleDate").find(".//Year").text
-
-    journal_title = root.find(".//Journal/Title").text
-    reported_doi = root.find(".//ELocationID").text
-
-    pre="pubmed:"
-
-    bibtex = f"""
-@misc{{{pre}{pmid},
-    author = {{{authors}}},
-    title = {{"{title}"}},
-    year = "{year}",
-
-    journal={{{journal_title}}},
-    doi={{{reported_doi}}},
-
-    url = "http://www.ncbi.nlm.nih.gov/pubmed/{pmid}"
-}}
-    """
-    eprint(bibtex)
-    return bibtex
 
 
 def normalize_citeid_to_pmid(citeid):
@@ -64,25 +30,7 @@ def normalize_citeid_to_pmid(citeid):
 
     return None
 
-def pmid_needed(pmid):
-    return not bib.is_existing_ref(pmid)
-
-def pubmed_filter(elem, doc):
-    if type(elem) == Cite:
-        for citation in elem.citations:
-            if pmid := normalize_citeid_to_pmid(citation.id):
-                prefixed_pmid = "pubmed:" + pmid
-                if pmid_needed(prefixed_pmid):
-                    eprint("Resolving PMID", pmid)
-                    bib.push_to_bib(pmid_to_bibtex(pmid))
-                    bib.push_new_ref(prefixed_pmid)
-
-                citation.id=prefixed_pmid
-
-    return elem
-
 def pmid_to_doi(pmid):
-    eprint("pmid_to_doi", pmid)
     root = pmid_to_xml(pmid)
 
     return root.find(".//PubmedData//ArticleIdList//ArticleId[@IdType='doi']").text
